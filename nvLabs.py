4ee639c07fb11d8519f67ab630c99c41f109c4ee
grahamlaye <graham.laye@qredo.com>
2024-02-04 17:52:25 +0000

    Progress bar.

--- nvLabs.py
+++ nvLabs.py
@@ -1,6 +1,8 @@
-import httpx, asyncio
+import httpx, asyncio, time
 import nvSecrets
 from pprint import pprint as pp
+from rich import print
+from rich.progress import Progress
 
 class NirvanaLabs:
     def __init__(self):
@@ -15,55 +17,60 @@ class NirvanaLabs:
             'jsonrpc': '2.0', 'method': None, 'params': None, 'id': 1
         }
 
-    async def requestTemplate(self, httpMethod, paramMethod, params=None):
+    async def requestTemplate(self, httpMethod, paramMethod, params=None, progress=None):
         self.payload['method'] = paramMethod
         if params:
             self.payload['params'] = params
         try:
+            task = progress.add_task(f"[cyan]{paramMethod} in progress...", total=1)
             response = await self.client.request(
-            httpMethod,
-            url=self.url,
-            headers=self.headers,
-            json=self.payload,
-            timeout=10
+                httpMethod,
+                url=self.url,
+                headers=self.headers,
+                json=self.payload,
+                timeout=10
             )
             response.raise_for_status()
+            progress.update(task, completed=1)
             return paramMethod, response.json()
         except httpx.HTTPError as e:
             print(f'There was an HTTP error in the API response: {e}')
             return None
 
-    async def getNetVersion(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='net_version')
+    async def getNetVersion(self, progress):
+        return await self.requestTemplate(httpMethod='POST', paramMethod='net_version', progress=progress)
+
+    async def getBlockNumber(self, progress):
+        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_blockNumber', progress=progress)
     
-    async def getBlockNumber(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_blockNumber')
+    async def getLatestBlockTxNumber(self, progress):
+        return await self.requestTemplate \
+            (httpMethod='POST', paramMethod='eth_getBlockTransactionCountByNumber', params=['latest'], progress=progress)
     
-    async def getLatestBlockTxNumber(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_getBlockTransactionCountByNumber', params=['latest'])
+    async def gasPrice(self, progress):
+        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_gasPrice', progress=progress)
     
-    async def gasPrice(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_gasPrice')
+    async def maxPriorityPerGas(self, progress):
+        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_maxPriorityFeePerGas', progress=progress)
     
-    async def maxPriorityPerGas(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_maxPriorityFeePerGas')
-    
-    async def getUncles(self):
-        return await self.requestTemplate(httpMethod='POST', paramMethod='eth_getUncleCountByBlockNumber', params=['latest'])
+    async def getUncles(self, progress):
+        return await self.requestTemplate \
+            (httpMethod='POST', paramMethod='eth_getUncleCountByBlockNumber', params=['latest'], progress=progress)
     
     async def runAll(self):
-        coroutines = [
-            self.getNetVersion(),
-            self.getBlockNumber(),
-            #self.getLatestBlockTxNumber(),
-            self.gasPrice(),
-            self.maxPriorityPerGas(),
-            self.getUncles()
+        with Progress() as progress:
+            coroutines = [
+                self.getNetVersion(progress),
+                self.getBlockNumber(progress),
+                #self.getLatestBlockTxNumber(progress),
+                self.gasPrice(progress),
+                self.maxPriorityPerGas(progress),
+                self.getUncles(progress)
             ]
-        results = await asyncio.gather(*coroutines)
-        for param_method, response in results:
-            print(f'Client ID: {id(self.client)} | {param_method}: {response}')
-        return results
-    
+            results = await asyncio.gather(*coroutines)
+            for param_method, response in results:
+                print(f'Client ID: {id(self.client)} | {param_method}: {response}')
+            return results
+
 if __name__ == '__main__':
     asyncio.run(NirvanaLabs().runAll())
